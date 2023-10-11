9c9f6aec7124297dac3a0dd5bfb15789326f0e91
Premraj Paul <94243441+Premraj12345@users.noreply.github.com>
2023-10-11 22:09:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ if input:
       os.system(f"cp {filename} files")
     os.system("zip -r hosanna_songs.zip files")
     start = False
-  if !start:
+  if start == False:
     st.download_button("Download Now","hosanna_songs.zip")
