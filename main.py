2c2fe22ce53f1b708d8be2c88e5a4c634fce0706
Premraj Paul <94243441+Premraj12345@users.noreply.github.com>
2023-10-11 22:05:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 st.header("Spotify Playlist Downloader")
 input = st.text_input("Paste the Link here")
 
-if os.exists("files") == False:
+if os.path.exists("files") == False:
   os.mkdir("files")
 
 if input == True:
